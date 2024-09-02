8b62974642522053c9731b5d62710793c98aa7e6
FoxPower <foxpower@gmail.com>
2024-09-02 13:58:37 -0500

    #001 - Carga Inicial

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import RouterLink from '@/components/UI/RouterLink.vue';
+
+    defineProps({
+        titulo: {
+            type: String
+        }
+    })
+
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink to="agregar-cliente">
+                Agregar Cliente
+            </RouterLink>
+        </div>
+        <h2 class="text-4xl font-extrabold text-slate-500">{{titulo}}</h2>
+    </div>
+    
+
+</template>
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,25 @@
+<script setup>
+        import RouterLink from '@/components/UI/RouterLink.vue';
+
+        defineProps({
+        titulo: {
+            type: String
+        }
+    })
+    
+</script>
+
+<template>
+
+<div>
+    <div class="flex justify-end">
+        <RouterLink to="inicio">
+                Volver
+        </RouterLink>
+    </div>
+    
+    <h2 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h2>
+</div>
+
+
+</template>
